e49a1f562ff5624aff2e06175abbf43fc76994b8
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-09-13 11:36:15 +0300

    feat: replace Instagram grid with Swiper slider
    - Replaced static grid of instagramPosts with Swiper slider using Swiper and SwiperSlide components
    - Enabled Autoplay and Pagination modules
    - Imported swiper/css and swiper/css/pagination stylesheets
    - Posts now display as a horizontal, automatically sliding carousel
    - Fully responsive with pagination dots for user control
    - Improves UI/UX by providing a dynamic, interactive Instagram-style feed

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,6 +6,10 @@ import { Dictionary } from "@/types/dictionary";
 import { counters } from "@/services/data/counters";
 import { CounterItem } from "@/types/counter";
 import { instagramPosts } from "@/services/data/instagramPosts";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 interface AboutProps {
   dict: Dictionary;
@@ -54,23 +58,39 @@ export default function About({ dict }: AboutProps) {
         {/* Instagram Style Posts */}
         <div className="about__instagram">
           <h3 className="about__instagram-title">Traveler Stories</h3>
-          <div className="about__instagram-grid">
-            {instagramPosts.map((post) => (
-              <div className="about__instagram-post" key={post.id}>
-                <div className="about__instagram-header">
-                  <span className="about__instagram-username">
-                    @{post.username}
-                  </span>
-                  <span className="about__instagram-location">
-                    {post.location}
-                  </span>
-                </div>
-                <div className="about__instagram-image">
-                  <Image src={post.image} alt={post.caption} />
-                </div>
-                <div className="about__instagram-caption">{post.caption}</div>
-              </div>
-            ))}
+          <div className="about__instagram-slider">
+            <Swiper
+              modules={[Autoplay, Pagination]}
+              spaceBetween={32}
+              slidesPerView={4}
+              pagination={{ clickable: true }}
+              breakpoints={{
+                0: { slidesPerView: 1 },
+                600: { slidesPerView: 2 },
+                900: { slidesPerView: 3 },
+              }}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+            >
+              {instagramPosts.map((post) => (
+                <SwiperSlide className="about__instagram-post" key={post.id}>
+                  <div className="about__instagram-header">
+                    <span className="about__instagram-username">
+                      @{post.username}
+                    </span>
+                    <span className="about__instagram-location">
+                      {post.location}
+                    </span>
+                  </div>
+                  <div className="about__instagram-image">
+                    <Image src={post.image} alt={post.caption} />
+                  </div>
+                  <div className="about__instagram-caption">{post.caption}</div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
 
